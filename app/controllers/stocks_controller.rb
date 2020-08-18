4236bfe2c0e3884fb2c18b83361df0040e51dd3f
mmtakeuchi <mikemtaki@gmail.com>
2020-08-18 11:50:43 -0700

    update stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,6 @@
 class StocksController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_stock, only: [:show, :edit, :update]
 
     def index
         @stocks = Stock.all
@@ -19,10 +20,24 @@ class StocksController < ApplicationController
     end
 
     def show
-        @stock = Stock.find_by_id(params[:id])
+    end
+
+    def edit
+    end
+
+    def update
+        if @stock.update(stock_params)
+            redirect_to stock_path(@stock)
+        else
+            render :edit
+        end
     end
 
     private
+
+    def set_stock
+        @stock = Stock.find_by_id(params[:id])
+    end
 
     def stock_params
         params.require(:stock).permit(:name, :ticker, :price, :cost, :shares)

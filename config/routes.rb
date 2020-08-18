a20b28bd9d663773c34c122807282ec1e5646475
mmtakeuchi <mikemtaki@gmail.com>
2020-08-18 10:41:19 -0700

    facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :notes
-  devise_for :users
   resources :stocks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_164957) do
+ActiveRecord::Schema.define(version: 2020_08_18_173733) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
@@ -38,8 +38,12 @@ ActiveRecord::Schema.define(version: 2020_08_18_164957) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
 end

095a67f3ef8656bb15f595ee5699d8b103003a33
mmtakeuchi <mikemtaki@gmail.com>
2020-08-18 23:56:44 -0700

    new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,27 @@
 class CategoriesController < ApplicationController
     before_action :current_user
+ 
 
     def index
         @categories = Category.all
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        
+        if @category.save
+            redirect_to categories_path
+        else
+            flash[:error] = "Category already exists"
+            render :new
+        end
+    end
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,13 @@
 class Category < ApplicationRecord
     has_many :stock_categories
     has_many :stocks, through: :stock_categories
+    
+    validates :name, uniqueness: true
+    before_validation :capitalize_name, on: [ :create, :update ]
+    
+    private
+
+    def capitalize_name
+      self.name = name.capitalize
+    end
 end
